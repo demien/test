b5d4b1a55b15257ec66f6778a3ac006f9c45b100
demien <demien@appannie.com>
2016-03-03 22:33:14 +0800

    wip

---
+++ casper/init.sh
@@ -0,0 +1,2 @@
+sudo ln -s /Users/demien/Documents/git_projects/test/casper/phantomjs-2.1.1-macosx/bin/phantomjs phantomjs
+sudo ln -s /Users/demien/Documents/git_projects/test/casper/n1k0-casperjs-54a7a05/bin/casperjs casperjs

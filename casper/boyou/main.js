b3120bfdd4a7a807ea8911477d4f9796a25c4ccb
demien <demien@appannie.com>
2016-05-03 14:12:17 +0800

    WIP

--- casper/boyou/main.js
+++ casper/boyou/main.js
@@ -7,44 +7,52 @@ var utils = require('utils');
 var G = window;
 G.shops = [];
 
-shop_url_list = 'http://www.boyouwang.com/news/line/0-0-102176-0-0-1-1.html'
-detail_url = 'http://www.boyouwang.com/line/product-4942/'
+shop_url_list = ['http://www.boyouwang.com/news/line/0-0-102176-0-0-1-1.html', 'http://www.boyouwang.com/news/line/0-0-102175-0-0-1-1.html']
 
 casper.start();
 
-casper.open(shop_url_list);
-
 casper.then(function(){
-    var urls = this.evaluate(function(){
-        var css_selector_shop_li = ".product_ul li";
-        var css_urls = $(css_selector_shop_li);
-        var urls = []
-        for (i=0; i<css_urls.length; i++){
-            url = $(css_urls[i]).find('.nr').find('a')[0].href;
-            urls.push(url)
-        }
-        return urls
-    })
-    G.shop_urls = urls
-});
 
-casper.then(function(){
-    G.shop_urls.map(function(url){
+    shop_url_list.map(function(url_list){
+           casper.open(url_list);
+
         casper.then(function(){
-            casper.open(url);
-            utils.dump(url);
-            casper.then(function(){
-                shop = {}
-                var title = this.evaluate(function(){
-                    var css_selector_title = "h1"
-                    title = $(css_selector_title)[0].textContent.trim().split("\n")[0];
-                    return title;
-                });
-                shop['url'] = url
-                shop['title'] = title
-                G.shops.push(shop)
+            var urls = this.evaluate(function(){
+                var css_selector_shop_li = ".product_ul li";
+                var css_urls = $(css_selector_shop_li);
+                var urls = []
+                for (i=0; i<css_urls.length; i++){
+                    url = $(css_urls[i]).find('.nr').find('a')[0].href;
+                    urls.push(url)
+                }
+                return urls
             })
-        })
+            G.shop_urls = urls;
+        });
+
+        casper.then(function(){
+            G.shop_urls.map(function(url){
+                casper.then(function(){
+                    casper.open(url);
+                    utils.dump(url);
+                    casper.then(function(){
+
+                        var shop = this.evaluate(function(){
+                            var shop = {}
+                            var css_selector_title = "h1"
+                            shop['title'] = $(css_selector_title)[0].textContent.trim().split("\n")[0];
+
+                            var css_selector_images = ".jcarousel-clip ul li a img"
+                            shop['images'] = $.map($(css_selector_images),function(foo){return foo.src;})
+                            return shop
+                        });
+
+                        shop['url'] = url
+                        G.shops.push(shop);
+                    })
+                })
+            })
+        });
     })
 })
 
